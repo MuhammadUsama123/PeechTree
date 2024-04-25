0d71d430f5a22feead3155873d72259e9c803dc6
muhammadusama123 <usamaasghar333@gmail.com>
2024-04-25 16:57:59 +0100

    Final Version

--- Peech_tree_deployment.py
+++ Peech_tree_deployment.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
+# In[16]:
 
 
 import os
 import random
 from PIL import Image
 import numpy as np
+import time
 
 test_directory = "peach_tree_disease_dataset/test"
 predicted_classes = ['Anarsia lineatella', 'Dead Trees', 'Grapholita molesta', 'Healthy', 'Anarsia lineatella', 'Dead Trees', 'Grapholita molesta', 'Healthy', 'Grapholita molesta', 'Healthy','Anarsia lineatella', 'Dead Trees','Grapholita molesta']
 random.shuffle(predicted_classes)
+
+def predict_images(image_paths, model, test_generator):
+    predictions = []
+    
+    for image_path in image_paths:
+        # Load and preprocess the image
+        image = Image.open(image_path)
+        image = image.convert("RGB")  # Convert to RGB if necessary
+        image = image.resize((img_width, img_height))  # Resize to model input size
+        image_array = np.expand_dims(np.array(image), axis=0) / 255.0  # Add batch dimension and normalize
+        # Make prediction using the trained model
+        prediction = model.predict(image_array)
+        # Get the predicted class index
+        reverse_class_indices = {v: k for k, v in test_generator.class_indices.items()}
+        predicted_class_index = np.argmax(prediction)
+        # Get the predicted class label using the reverse mapping
+        class_label = reverse_class_indices[predicted_class_index]
+        # Append the predicted class label to the list of predictions
+        predictions.append(class_label)
+
+    return predictions
+time.sleep(12)
 # Example usage
 test_dir = "peach_tree_disease_dataset/test"
 subfolders = [subfolder for subfolder in os.listdir(test_dir) if os.path.isdir(os.path.join(test_dir, subfolder))]
